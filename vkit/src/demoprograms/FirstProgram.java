91a52332d290a72839c7d3ca55ed387fab15809e
VINAY KUMAR H N <VINAY KUMAR H N@DESKTOP-E3T5M58>
2022-04-19 22:19:42 +0530

    demoprograms

---
+++ vkit/src/demoprograms/FirstProgram.java
@@ -0,0 +1,9 @@
+package demoprograms;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+
+
+}
---
+++ vkit/src/demoprograms/hello.java
@@ -0,0 +1,18 @@
+package demoprograms;
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		int x=5;
+		if(x!=5)
+		{
+			System.out.println("value of x is not 5");
+		}
+		else
+		{
+			System.out.println("value of x is 5");
+		}
+	}
+
+}
